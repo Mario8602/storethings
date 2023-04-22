8cb51e1f52e3b4d71494c847e300f2d8bb18f633
Mario8602 <hellp8901@gmail.com>
2023-04-22 20:53:23 +0500

    Commit #25.2 Улучшено отображение фильтров для поиска товаров

--- store/product/filters.py
+++ store/product/filters.py
@@ -1,4 +1,8 @@
+from django import forms
 import django_filters
+# from django_filters.widgets import LookupChoiceWidget
+from django_filters.widgets import BooleanWidget
+
 from .models import Product, MonitorDetails, LaptopDetails, KeyboardDetails, VideoCardDetails
 
 
@@ -30,22 +34,40 @@ class LaptopFilter(django_filters.FilterSet):
 
 class KeyboardFilter(django_filters.FilterSet):
 
-    # product__price = django_filters.RangeFilter()
-    # class Meta:
-    #     model = KeyboardDetails
-    #     fields = ['product__price', 'color', 'key_backlight', 'keycap_material', 'programmable_keys', 'case_material', 'water_protection']
+    KEYBOARD_TYPE_CHOICE = (
+        ('', 'Выберите тип клавиатуры'),
+        ('Мембранная', 'Мембранная'),
+        ('Резиновая', 'Резиновая'),
+        ('Механическая', 'Механическая'),
+        ('Оптическая', 'Оптическая'),
+    )
+
+    key_backlight = django_filters.BooleanFilter(
+        label='Подсветка клавиш',
+        widget=BooleanWidget(attrs={
+            'class': 'md-textarea form-control lb-form',
+        }),
+    )
+    keyboard_type = django_filters.ChoiceFilter(
+        choices=KEYBOARD_TYPE_CHOICE,
+        widget=forms.Select(attrs={
+            'class': 'filter-form lb-form',
+        }),
+        empty_label=None
+    )
 
     class Meta:
         model = KeyboardDetails
-        fields = {
-            'product__price': ['lt', 'gt'],
-            'color': ['exact'],
-            'key_backlight': ['exact', 'contains'],
-            'keycap_material': ['exact', 'contains'],
-            'programmable_keys': ['contains'],
-            'case_material': ['contains'],
-            'water_protection': ['contains'],
-        }
+        fields = ['key_backlight', 'keyboard_type']
+    #     fields = {
+    #         'product__price': ['lt', 'gt'],
+    #         'color': ['exact'],
+    #         # 'key_backlight': ['exact',],
+    #         'keycap_material': ['exact',],
+    #         'programmable_keys': ['contains'],
+    #         'case_material': ['contains'],
+    #         'water_protection': ['contains'],
+    #     }
 
 
 class VideoCartFilter(django_filters.FilterSet):
@@ -53,3 +75,6 @@ class VideoCartFilter(django_filters.FilterSet):
     class Meta:
         model = VideoCardDetails
         fields = ['color', 'videomemory_size', 'memory_type', 'frequency_videochip']
+
+
+# class FrequencyWidget(LookupChoiceWidget):
